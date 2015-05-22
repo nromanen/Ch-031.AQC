d4fa13f5bab1d7e2a6ae90b9619d8b5639af8070
white-buddah <l3ctr050ul@gmail.com>
2015-05-22 14:16:22 +0300

    Added tool for table values checking

---
+++ OMS_test/src/main/java/tools/CheckTableValue.java
@@ -0,0 +1,49 @@
+package tools;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+import java.util.ArrayList;
+
+public class CheckTableValue {
+    private WebDriver driver;
+    public CheckTableValue(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String findValue(String id, String th,int tdId) throws javax.script.ScriptException{
+/*        String mas = (driver.findElement(By.xpath("//div[@id='"+id+"']//table//td["+td+"]//text()"))).toString();
+        List<String> list = Arrays.asList(mas.split("\\s*,\\s*"));
+        String[] value = new String[list.size()];
+        list.toArray(value);
+        return value[tdId];*/
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+
+        String js ="function find(){"+
+                "var table = document.getElementById(\""+id+"\").getElementsByTagName(\"table\")[0];"+
+                "var searchText = \""+th+"\";"+
+                "var found;"+
+
+                "for (var i = 0; i < table.rows[0].cells.length; i++) {"+
+                "   if (table.rows[0].cells[i].outerText == searchText) {"+
+
+                "   found = table.rows[0].cells[i];"+
+                "  console.log(found);"+
+                " count = i;"+
+                "console.log(count);"+
+                "break;"+
+                "}}"+
+
+                "var td = [];"+
+                "for (var j= 1; j<table.rows.length; j++) {"+
+                "var el = table.rows[j].cells[count].outerText;"+
+                "td.push(el);"+
+                "}"+
+                "return td;}"+
+                "return find();";
+        Object obj = (Object) executor.executeScript(js);
+        ArrayList value = new ArrayList();
+        value = (ArrayList) obj;
+        return (String)value.get(tdId);
+    }
+}
