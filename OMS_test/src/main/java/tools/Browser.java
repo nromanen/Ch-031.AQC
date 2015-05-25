96aaae1629e71869c601d6a0393007049cb9538f
white-buddah <l3ctr050ul@gmail.com>
2015-05-25 14:32:48 +0300

    TIME_OUT_SECONDS

--- OMS_test/src/main/java/tools/Browser.java
+++ OMS_test/src/main/java/tools/Browser.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Browser {
     private WebDriver driver;
-    private int timeOutSeconds = 10;
+    private int TIME_OUT_SECONDS = 10;
 
     public Browser(WebDriver driver) {
         this.driver = driver;
@@ -20,44 +20,44 @@ public class Browser {
     }
 
     public WebElement findElementByName(String locator) {
-        WebElement element = (new WebDriverWait(driver, timeOutSeconds))
+        WebElement element = (new WebDriverWait(driver, TIME_OUT_SECONDS))
                 .until(ExpectedConditions.presenceOfElementLocated(By.name(locator)));
         return element;
     }
 
     public WebElement findElementById(String locator) {
-        WebElement element = (new WebDriverWait(driver, timeOutSeconds))
+        WebElement element = (new WebDriverWait(driver, TIME_OUT_SECONDS))
                 .until(ExpectedConditions.presenceOfElementLocated(By.id(locator)));
         return element;
     }
 
     public WebElement findElementByLinkText(String locator) {
-        WebElement element = (new WebDriverWait(driver, timeOutSeconds))
+        WebElement element = (new WebDriverWait(driver, TIME_OUT_SECONDS))
                 .until(ExpectedConditions.presenceOfElementLocated(By.linkText(locator)));
         return element;
     }
 
     public WebElement findElementByCssSelector(String locator) {
-        WebElement element = (new WebDriverWait(driver, timeOutSeconds))
+        WebElement element = (new WebDriverWait(driver, TIME_OUT_SECONDS))
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(locator)));
         return element;
     }
 
     public WebElement findElementByXpath(String locator) {
-        WebElement element = (new WebDriverWait(driver, timeOutSeconds))
+        WebElement element = (new WebDriverWait(driver, TIME_OUT_SECONDS))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
         return element;
     }
 
 
     public WebElement findElementByClassName(String locator) {
-        WebElement element = (new WebDriverWait(driver, timeOutSeconds))
+        WebElement element = (new WebDriverWait(driver, TIME_OUT_SECONDS))
                 .until(ExpectedConditions.presenceOfElementLocated(By.className(locator)));
         return element;
     }
 
     public WebElement findElementByPartialLinkText(String locator) {
-        WebElement element = (new WebDriverWait(driver, timeOutSeconds))
+        WebElement element = (new WebDriverWait(driver, TIME_OUT_SECONDS))
                 .until(ExpectedConditions.presenceOfElementLocated(By.partialLinkText(locator)));
         return element;
     }
