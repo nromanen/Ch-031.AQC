892378c08935e9ab6015af17ca647a3807e698e0
white-buddah <l3ctr050ul@gmail.com>
2015-06-03 18:41:22 +0300

    Finished all Pages and few tests

--- OMS_test/src/test/java/ordering/TestEditOrderPage.java
+++ OMS_test/src/test/java/ordering/TestEditOrderPage.java
@@ -4,23 +4,17 @@ import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.junit.*;
 import pages.auth.LoginPage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.auth.UserInfoPage;
 import pages.ordering.AddItemPage;
 import pages.ordering.EditOrderPage;
 import pages.ordering.OrderPage;
-import tools.Browser;
 import tools.CheckTableValue;
 import tools.DBUnitConfig;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class TestEditOrderPage extends DBUnitConfig{
     private static WebDriver driver;
     private static String BASE_URL = "http://localhost:8080/OMS2";
-    EditOrderPage editOrderPage;
     String LOGIN = "cust";
     String PASS = "pass";
 
