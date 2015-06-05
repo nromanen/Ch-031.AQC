3b9b50c2c2abb2d068257a08a084603857108a1a
Alexander <l3ctr050ul@gmail.com>
2015-06-05 16:53:00 +0300

    Update import.sql.
    Update import.sql to the right version.

--- OMS_test/src/main/resources/import.sql
+++ OMS_test/src/main/resources/import.sql
@@ -27,12 +27,10 @@ insert into CustomerTypes(Discount,MaxRange,MinRange,TypeName) values(0,3000,100
 insert into CustomerTypes(Discount,MaxRange,MinRange,TypeName) values(0,10000,3000,'Gold');
 insert into CustomerTypes(Discount,MaxRange,MinRange,TypeName) values(0,null,10000,'Platinum');
 
-insert into Users(IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(true,0,'marytest','Mary','S.','customer1','qwerty',1,1,4);
+insert into Users(ID, IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(1, true,0,'nellytest','Nelly','Moore','admin1','qwerty',1,1,1);
+insert into Users(ID, IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(2, true,0,'bobtest','Bob','B.','merch1','qwerty',2,2,2);
+insert into Users(ID, IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(3, true,0,'anntest','Ann','Lee','supervisor1','qwerty',1,3,3);
+insert into Users(ID, IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(4, true,0,'marytest','Mary','S.','customer1','qwerty',1,1,4);
+insert into Users(ID, IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(5, true,0,'nellytest','Nelly','Moore','admin','pass',1,1,1);
 
-insert into Users(IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(true,0,'bobtest','Bob','B.','merch1','qwerty',2,2,2);
-
-insert into Users(IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(true,0,'anntest','Ann','Lee','supervisor1','qwerty',1,3,3);
-
-insert into Users(IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(true,0,'nellytest','Nelly','Moore','admin1','qwerty',1,1,1);
-insert into Users(IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(true,0,'nellytest','Nelly','Moore','admin','pass',1,1,1);
 
