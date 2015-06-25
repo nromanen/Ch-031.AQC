2a80a9308f491c1424051e3cf40b9d83aeb8d50b
olesia805 <lesia805@gmail.com>
2015-06-25 10:53:16 +0300

    Update CustomerAddProductsToOrderPage.java

--- OMS_test/src/main/java/pages/ordering/CustomerAddProductsToOrderPage.java
+++ OMS_test/src/main/java/pages/ordering/CustomerAddProductsToOrderPage.java
@@ -1,17 +1,18 @@
 package pages.ordering;
 
 import java.util.ArrayList;
-
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import tools.Browser;
 
 /**
- * 
+ * This class describe 'Add item' page (of 'Ordering' tab for Customer) functionality
+ * and provides a way to use it.
  * @author Olesia
  *
  */
@@ -20,7 +21,8 @@ public class CustomerAddProductsToOrderPage {
 
 	private WebDriver driver;
 	private Browser browser;
-	private static final String VALUE_OF_DONE_BUTTON = "Done"; 
+	private static final String DONE_BUTTON_VALUE_LOCATOR  = "Done"; 
+	private static final String QUANTITY_FIELD_ID_LOCATOR = "quantity";
 
 	public CustomerAddProductsToOrderPage(WebDriver driver) {
 		this.driver = driver;
@@ -44,19 +46,23 @@ public class CustomerAddProductsToOrderPage {
 		}
 		return names;
 	}
-    
-    public void selectProduct() {
-	
-    	browser.findElementByCssSelector("#selectFrom1 > a").click();
-	}
-
-	public void selectInitProduct() {
+   
+    public void selectInitProduct() {
 		browser.findElementByLinkText("Select").click();
 	}
     
+    public String findNameOfSelectedProduct(){    
+		
+    	return browser.findElementByXpath("//form[@id = 'doneForm']/table/tbody/tr[1]/td[2]").getText();
+	}
+    
+    public String findPriceOfSelectedProduct(){    
+		
+    	return browser.findElementByXpath("//form[@id = 'doneForm']/table/tbody/tr[3]/td[2]").getText();
+	}
+    
     public CustomerCreateOrderPage clickDoneButton() { 
-    	browser.findElementByXpath("//input[@type='submit'][@value = '"+VALUE_OF_DONE_BUTTON+"']").click();  
+    	browser.findElementByXpath("//input[@type='submit'][@value = '"+DONE_BUTTON_VALUE_LOCATOR+"']").click();  
     	return new CustomerCreateOrderPage(driver);  	
     }
-    
 }
