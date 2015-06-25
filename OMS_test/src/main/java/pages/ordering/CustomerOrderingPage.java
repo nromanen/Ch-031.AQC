ae965ad3a9b5f3a17a3e15e25af6b804258913aa
olesia805 <lesia805@gmail.com>
2015-06-25 10:59:54 +0300

    Update CustomerOrderingPage.java

--- OMS_test/src/main/java/pages/ordering/CustomerOrderingPage.java
+++ OMS_test/src/main/java/pages/ordering/CustomerOrderingPage.java
@@ -9,7 +9,8 @@ import org.openqa.selenium.WebElement;
 
 import tools.Browser;
 /**
- * 
+ * This class describe 'Ordering' page (tab) for Customer functionality
+ * and provides a way to use it. 
  * @author Olesia
  *
  */
@@ -20,6 +21,9 @@ public class CustomerOrderingPage {
 	private Browser browser; 
 
 	private static final String LINK_FOR_CREATING_NEW_ORDER = "Create new order"; 
+	private static final String LINK_FOR_SHOW_10_ITEMS = "Show 10 items"; 
+	private static final String LINK_FOR_SHOW_5_ITEMS = "Show 5 items"; 
+	private static final String TAG_NAME_OF_TABLE_ROW = "tr";
 	
 	public CustomerOrderingPage(WebDriver driver) {
 		this.driver = driver;
@@ -50,4 +54,32 @@ public class CustomerOrderingPage {
 		return names;
 	}
 	
-}
+	public void clickShowTenItems(){  
+		
+		browser.findElementByLinkText(LINK_FOR_SHOW_10_ITEMS).click();	
+	}
+	
+    public void clickShowFiveItems(){  
+		
+		browser.findElementByLinkText(LINK_FOR_SHOW_5_ITEMS).click();	
+	}
+	
+    public boolean findShowTenItemsLink(){     
+		
+		 if (browser.findElementByLinkText(LINK_FOR_SHOW_10_ITEMS).isDisplayed()) return true ;
+				 else return false;
+	}
+    
+    public boolean findShowFiveItemsLink(){     
+		
+		 if (browser.findElementByLinkText(LINK_FOR_SHOW_5_ITEMS).isDisplayed()) return true ;
+				 else return false;
+	}
+    
+    public int getCountOfOrdersInTable() { 
+    
+    		List<WebElement> rows = browser.findElementsByTagName(TAG_NAME_OF_TABLE_ROW); 
+   		 return rows.size() - 2;
+    }
+    
+  }
