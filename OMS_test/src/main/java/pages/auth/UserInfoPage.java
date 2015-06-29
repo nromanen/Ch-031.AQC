8240fd257e655483cbb27098f419abe0241e44cf
olesia805 <lesia805@gmail.com>
2015-06-30 00:35:23 +0300

    Update UserInfoPage.java

--- OMS_test/src/main/java/pages/auth/UserInfoPage.java
+++ OMS_test/src/main/java/pages/auth/UserInfoPage.java
@@ -15,6 +15,7 @@ public class UserInfoPage extends BasePage{
     private static final String USERS_LOCATOR = "Administration";
     private static final String ORDERING = "Ordering";
     private static final String LINK_FOR_ORDERING = "Ordering";  
+    private static final String USER_INFO_TEXT_LOCATOR = "legend";
 
 
     public UserInfoPage(WebDriver driver) {
@@ -41,8 +42,13 @@ public class UserInfoPage extends BasePage{
         browser.findElementByLinkText(ORDERING).click();
         return new OrderPage(browser.getDriver());
     }
-
-
-
+    
+     public String findLink(String link){
+    	return browser.findElementByLinkText(link).getText();
+    }
+    
+    public String findUserInfoText(){
+    	return browser.findElementByTagName(USER_INFO_TEXT_LOCATOR).getText();
+    }
 
 }
